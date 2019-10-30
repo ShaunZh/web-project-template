92d7e074b7932757a8765f01455a450e7422a2f5
ShaunZh <308826842@qq.com>
2019-10-30 18:44:14 +0800

    add: 添加http请求的baseURL配置

--- new-vue-mobile/src/plugins/axios/config.js
+++ new-vue-mobile/src/plugins/axios/config.js
@@ -2,5 +2,5 @@
 export const AXIOS_DEFAULT_CONFIG = {
   timeout: 10000,
   'Content-Type': 'application/json',
-  baseURL: 'http://stretch.punan.xyz/mgr' // 测试
+  baseURL: process.env.VUE_APP_BASE_API // 测试
 }
