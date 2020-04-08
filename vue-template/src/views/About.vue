adf7ef31e4db9c113a4bc9180f50d7d7661c22d3
ShaunZh <308826842@qq.com>
2020-04-08 11:36:27 +0800

    test: rem layout

--- vue-template/src/views/About.vue
+++ vue-template/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <svg-icon icon-class="ic-experiment"></svg-icon>
   </div>
 </template>
+
+<style lang="scss">
+h1 {
+  font-size: 28px;
+}
+</style>
