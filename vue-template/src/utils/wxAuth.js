f92f55cfc463f42bd41d24504e0eadc56e6a85a1
ShaunZh <308826842@qq.com>
2020-04-10 17:17:31 +0800

    add: utils function

---
+++ vue-template/src/utils/wxAuth.js
@@ -0,0 +1,30 @@
+/*
+ * @Description:
+ * @Author: Hexon
+ * @Date: 2020-04-08 18:01:03
+ * @LastEditors: Hexon
+ * @LastEditTime: 2020-04-08 18:23:21
+ */
+
+import WxAuth from 'wxtoken'
+import locationSearch from '@/utils/locationSearch'
+import loginApi from '@/service/login'
+
+export const WxInstance = WxAuth.getInstance()
+
+export const authHttp = () => {
+  const searchParams = locationSearch(window.location.search)
+  return WxInstance.wxAuth(loginApi.wxGetJsapiSignature, loginApi.wxGetAuth, {
+    // auth：为调用微信授权接口的请求参数
+    auth: {
+      code: searchParams.code,
+      type: 'parent',
+      clientKey: searchParams.clientKey || ''
+    },
+    // signature: 为调用微信签名接口的请求参数
+    signature: {
+      clientKey: searchParams.clientKey || '',
+      url: window.location.href.split('#')[0]
+    }
+  })
+}
