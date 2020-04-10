284c1080ed70834406f911af53d511cbd538d9af
ShaunZh <308826842@qq.com>
2020-04-10 17:44:17 +0800

    add: detail 页面

--- vue-template/src/views/list/detail.vue
+++ vue-template/src/views/list/detail.vue
@@ -3,5 +3,10 @@
  * @Author: Hexon
  * @Date: 2020-04-09 14:40:49
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-09 14:40:49
+ * @LastEditTime: 2020-04-10 17:33:12
  -->
+<template>
+  <div class="page-list-detail">
+    详情页面
+  </div>
+</template>
