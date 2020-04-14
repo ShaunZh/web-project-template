70d2ffdd7a04fe6a4a34a6a0a32fa975a12a0fb2
ShaunZh <308826842@qq.com>
2020-04-14 18:50:12 +0800

    update: list create page

--- vue-template/src/views/list/create.vue
+++ vue-template/src/views/list/create.vue
@@ -3,5 +3,18 @@
  * @Author: Hexon
  * @Date: 2020-04-09 14:40:59
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-09 14:40:59
+ * @LastEditTime: 2020-04-14 14:19:10
  -->
+<template>
+  <div class="page-list-add">
+    <ListItemOperate operate="create"></ListItemOperate>
+  </div>
+</template>
+
+<script>
+import ListItemOperate from './components/ListItemOperate'
+export default {
+  name: 'ListItemAdd',
+  components: { ListItemOperate }
+}
+</script>
