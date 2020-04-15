461dfb1d1424e462c23435f4ba26c7bc4113c7f8
ShaunZh <308826842@qq.com>
2020-04-15 16:10:11 +0800

    update: rename page component

--- vue-template/src/views/list/create.vue
+++ vue-template/src/views/list/create.vue
@@ -3,10 +3,10 @@
  * @Author: Hexon
  * @Date: 2020-04-09 14:40:59
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-14 14:19:10
+ * @LastEditTime: 2020-04-14 18:49:54
  -->
 <template>
-  <div class="page-list-add">
+  <div class="page-list-create">
     <ListItemOperate operate="create"></ListItemOperate>
   </div>
 </template>
@@ -14,7 +14,7 @@
 <script>
 import ListItemOperate from './components/ListItemOperate'
 export default {
-  name: 'ListItemAdd',
+  name: 'ListItemCreate',
   components: { ListItemOperate }
 }
 </script>
