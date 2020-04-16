f7208c423096233c4fb43333be735d2eff97f273
ShaunZh <308826842@qq.com>
2020-04-16 17:20:38 +0800

    add: search 页面

--- vue-template/src/views/search/index.vue
+++ vue-template/src/views/search/index.vue
@@ -3,5 +3,34 @@
  * @Author: Hexon
  * @Date: 2020-04-09 14:44:24
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-09 14:44:25
+ * @LastEditTime: 2020-04-16 15:48:26
  -->
+<template>
+  <div class="page-search">
+    <van-search v-model="keywords" placeholder="请输入搜索关键词" show-action autofocus @search="onSearch">
+      <template v-slot:action>
+        <div @click="onSearch">搜索</div>
+      </template>
+    </van-search>
+  </div>
+</template>
+
+<script>
+import { Search as VanSearch } from 'vant'
+export default {
+  name: 'Search',
+  components: {
+    VanSearch
+  },
+  data() {
+    return {
+      keywords: ''
+    }
+  },
+  methods: {
+    onSearch() {
+      console.log('search')
+    }
+  }
+}
+</script>
