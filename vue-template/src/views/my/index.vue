66fe1653ec3ad6966db4801adb78df7cda1efd40
ShaunZh <308826842@qq.com>
2020-04-17 17:04:01 +0800

    add: 个人中心页面

--- vue-template/src/views/my/index.vue
+++ vue-template/src/views/my/index.vue
@@ -3,5 +3,11 @@
  * @Author: Hexon
  * @Date: 2020-04-09 14:47:53
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-09 14:47:54
+ * @LastEditTime: 2020-04-17 17:02:54
  -->
+
+<template>
+  <div class="page-my">
+    个人中心
+  </div>
+</template>
