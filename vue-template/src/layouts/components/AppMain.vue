2bc513d06b6138c93a5efe256677ace8502b5043
ShaunZh <308826842@qq.com>
2020-04-20 14:20:25 +0800

    add: 添加简单的路由过渡效果

--- vue-template/src/layouts/components/AppMain.vue
+++ vue-template/src/layouts/components/AppMain.vue
@@ -3,11 +3,11 @@
  * @Author: Hexon
  * @Date: 2020-04-09 15:29:40
  * @LastEditors: Hexon
- * @LastEditTime: 2020-04-13 15:24:35
+ * @LastEditTime: 2020-04-20 14:17:56
  -->
 <template>
   <section class="app-main">
-    <transition name="fade-transform" mode="out-in">
+    <transition name="fade">
       <keep-alive :include="keepAliveRoute">
         <router-view :key="key" />
       </keep-alive>
@@ -23,6 +23,7 @@ export default {
   },
   computed: {
     keepAliveRoute() {
+      console.log('keep alive route', this.$store.getters.keepAliveRoute)
       return this.$store.getters.keepAliveRoute
     },
     key() {
@@ -40,5 +41,13 @@ export default {
 .app-main {
   /* 给底部菜单留出位置，否则底部菜单会覆盖在内容上面 */
   padding-bottom: 40px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
